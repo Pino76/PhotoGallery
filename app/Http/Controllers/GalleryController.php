82e4bac0fadc5f652ae5a0766719dff8f46f8ef4
pinof <fattoruso.g@gmail.com>
2020-01-26 21:27:08 +0100

    gestione gallery

---
+++ app/Http/Controllers/GalleryController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Album;
+use App\Models\Photo;
+use Illuminate\Http\Request;
+
+class GalleryController extends Controller {
+
+    public function index(){
+        $albums = Album::latest()->get();
+        return view('gallery.albums' , ['albums' => $albums]);
+    }
+
+    public function showAlbumImages(Album $album){
+        $images = Photo::whereAlbumId($album->id)->latest()->get();
+        return view('gallery.images', ['images' => $images, 'album'=> $album ]);
+    }
+
+
+
+}
