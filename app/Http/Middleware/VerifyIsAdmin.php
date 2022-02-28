68a05f96b215ea2232de42102a079cada4a8adba
pinof <fattoruso.g@gmail.com>
2022-02-28 22:03:51 +0100

    middleware

---
+++ app/Http/Middleware/VerifyIsAdmin.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Auth;
+use Illuminate\Http\Request;
+
+class VerifyIsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(!Auth::user()->isAdmin()){
+            return redirect()->route('login');
+        }
+        return $next($request);
+    }
+}
---
+++ app/Listeners/NotifyAdminNewAlbum.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\NewAlbumCreated;
+use App\Models\User;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\NotifyAdminNewAlbum as NotifyAdmin;
+
+class NotifyAdminNewAlbum
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+
+
+
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  NewAlbumCreated  $event
+     * @return void
+     */
+    public function handle(NewAlbumCreated $event)
+    {
+        $admins = User::select(['name', 'email'])->where('user_role', 'admin')->get();
+        foreach($admins AS $admin){
+            Mail::to($admin->email)->send(new NotifyAdmin($admin, $event->album ));
+        }
+    }
+}
